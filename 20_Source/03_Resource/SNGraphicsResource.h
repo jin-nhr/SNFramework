d9eb6148a34b1d0736ea8460e8ea9538f276ce92
jin-nhr <shinji7773939@gmail.com>
2026-09-26 11:44:51 +0900

    GUIリソースのアトラス化 フォント関連のリソース定義修正

--- 20_Source/03_Resource/SNGraphicsResource.h
+++ 20_Source/03_Resource/SNGraphicsResource.h
@@ -10,29 +10,16 @@ enum SNGraphicsResID
 	// 起動リソース
 	SNGraphicsResStartupTop,
 
-	SNGraphicsResSystemFont = SNGraphicsResStartupTop,
+	SNGraphicsResGUI = SNGraphicsResStartupTop,
 	SNGraphicsResSystemBlack,
-	SNGraphicsResGUI,
 
-	SNGraphicsResStartupEnd = SNGraphicsResGUI,
+	SNGraphicsResStartupEnd = SNGraphicsResSystemBlack,
 	// -----------------------------------------------
 
 	// -----------------------------------------------
 	// システムリソース
 	SNGraphicsResSystemTop,
-
-	SNGraphicsResExFont01 = SNGraphicsResSystemTop,
-	SNGraphicsResExFont02,
-	SNGraphicsResExFont03,
-	SNGraphicsResExFont04,
-	SNGraphicsResExFont05,
-	SNGraphicsResExFont06,
-	SNGraphicsResExFont07,
-	SNGraphicsResExFont08,
-	SNGraphicsResExFont09,
-	SNGraphicsResExFont10,
-	SNGraphicsResExFont11,
-	SNGraphicsResStartLogo1,
+	SNGraphicsResStartLogo1 = SNGraphicsResSystemTop,
 	SNGraphicsResStartLogo2,
 	SNGraphicsResWindow,
 	SNGraphicsResButton,
@@ -66,24 +53,12 @@ public:
 	{
 		// -----------------------------------------------
 		// 起動リソース
-		(String)L"システムフォント.png", 
-		(String)L"BGBK.png",
 		(String)L"GUI.png",
+		(String)L"BGBK.png",
 		// -----------------------------------------------
 		
 		// -----------------------------------------------
 		// システムリソース
-		(String)L"拡張フォント01.png",
-		(String)L"拡張フォント02.png",
-		(String)L"拡張フォント03.png",
-		(String)L"拡張フォント04.png",
-		(String)L"拡張フォント05.png",
-		(String)L"拡張フォント06.png",
-		(String)L"拡張フォント07.png",
-		(String)L"拡張フォント08.png",
-		(String)L"拡張フォント09.png",
-		(String)L"拡張フォント10.png",
-		(String)L"拡張フォント11.png",
 		(String)L"起動ロゴ1.png",
 		(String)L"起動ロゴ2.png",
 		(String)L"ウインドウ.png",
